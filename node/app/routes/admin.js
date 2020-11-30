31129bddd750435ea84029a5e687d4bfa5061f82
brunosb02 <72226298+brunosb02@users.noreply.github.com>
2020-11-29 22:36:43 -0300

    Add files via upload

---
+++ node/app/routes/admin.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+	app.get('/formulario_inclusao_noticia', function(req, res){
+		app.app.controllers.adm.formulario_inclusao_noticia(app,req,res)
+	
+	});
+	
+	app.post('/noticias/salvar', function(req, res){
+		app.app.controllers.adm.noticia_salvar(app,req,res)
+		      
+	
+		
+	
+	});
+
+}
